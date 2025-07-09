4e655a627c6d9d47f5818d4048b8c220c1fa2a61
Akhil <akhil.kuchibhotla2006@gmail.com>
2025-07-09 22:41:12 +0530

    Time: 13 ms (19.04%), Space: 19.1 MB (99.83%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
         hashmap = {}
+        n = len(nums)
         for num in nums:
             if num in hashmap:
                 hashmap[num] += 1
             else:
                 hashmap[num] = 1
             if hashmap[num] > n // 2:
-                return num
-        
+                    return num
+            
         
