db125beb651d13ecd3a810040f88344ba963dab8
Akhil <akhil.kuchibhotla2006@gmail.com>
2025-07-09 10:29:36 +0530

    Time: 24 ms (55.38%), Space: 23.2 MB (93.78%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,9 +5,8 @@ class Solution:
         for i in range(len(nums)):
             answer[i] = prefix
             prefix *= nums[i]
-
         suffix = 1
-        for i in range(len(nums) - 1, -1 , -1):
-            answer[i] *= suffix
-            suffix  *= nums[i]
+        for i in range(len(nums) - 1 , -1 ,-1 ):
+            answer[i] *= suffix 
+            suffix *= nums[i]
         return answer
