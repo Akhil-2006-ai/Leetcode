c09294be69378cb3336409a0c81c82250284e26c
Akhil <akhil.kuchibhotla2006@gmail.com>
2025-07-10 10:49:28 +0530

    Time: 15 ms (41.98%), Space: 18 MB (32.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,22 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-         hashmaps = {}
-         hashmapt = {}
-
-         if len(s) != len(t):
+        hashmaps = {}
+        hashmapt = {}
+        if len(s) != len(t):
             return False
 
-         for i in range(len(s)):
-            hashmaps[s[i]] = 1 + hashmaps.get(s[i],0)
-            hashmapt[t[i]] = 1 + hashmapt.get(t[i],0)
+        for i in range(len(s)):
+            if s[i] in hashmaps:
+                hashmaps[s[i]] += 1
+            else:
+                hashmaps[s[i]] = 1
+       
         
-         if hashmaps == hashmapt:
+            if t[i] in hashmapt:
+                hashmapt[t[i]] += 1
+            else:
+                hashmapt[t[i]] = 1
+        if hashmaps == hashmapt:
             return True
-         return False
-
-
-
-
-
-
-             
+        else:
+            return False
+        
+         
