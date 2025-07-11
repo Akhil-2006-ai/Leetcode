59bbab5956430d225b723d29b9c5090b5ba1fba6
Akhil <akhil.kuchibhotla2006@gmail.com>
2025-07-11 15:02:05 +0530

    Time: 4 ms (40.57%), Space: 17.9 MB (10.64%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -6,10 +6,10 @@ class Solution:
         while L <= R:
             Mid = (L + R) // 2
             if Mid**2 < x:
-                L+= 1
+                L= Mid + 1
                 res = Mid
             elif Mid**2 > x:
-                R -= 1
+                R  = Mid - 1
             else:
                 return Mid
         return res
