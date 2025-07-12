e876f096d5e93db4b2e50c2b041f27aaec3cf9bd
Akhil <akhil.kuchibhotla2006@gmail.com>
2025-07-12 10:17:14 +0530

    Time: 531 ms (76.7%), Space: 20.5 MB (85.82%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
         nums.sort()
         res = []
-        for i , n in enumerate(nums):
-            M = i + 1
-            R = len(nums) - 1
+
+        for i, n in enumerate(nums):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
+            M = i + 1
+            R = len(nums) - 1
             while M < R:
                 Threesome = n + nums[M] + nums[R]
                 if Threesome < 0:
@@ -19,9 +19,7 @@ class Solution:
                     M += 1
                     while nums[M] == nums[M - 1] and M < R:
                         M += 1
-        return res
+        return res                    
+        
                 
-
-
-
         
