820c80162c268f67011827f216f59a4bdce1cd6c
Akhil <akhil.kuchibhotla2006@gmail.com>
2025-07-25 14:33:24 +0530

    Time: 41 ms (67.73%), Space: 18.4 MB (6.16%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -9,15 +9,17 @@ class Solution:
         def dfs(node1 , node2):
             if not node1 and not node2:
                 return True
-            if not  node1 or not node2 or node1.val != node2.val:
+            if not node1 or not node2 or node1.val != node2.val:
                 return False
             return dfs(node1.left , node2.left) and dfs(node1.right , node2.right)
 
         if not root:
             return False
-        if dfs(root,subRoot):
+
+        if dfs(root , subRoot):
             return True
         return self.isSubtree(root.left , subRoot) or self.isSubtree(root.right , subRoot)
+
 
 
         
