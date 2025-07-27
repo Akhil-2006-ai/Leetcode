5f7082220ff31f2ee7a10c82474fa51d8474f27d
Akhil <akhil.kuchibhotla2006@gmail.com>
2025-07-27 23:54:05 +0530

    Time: 0 ms (100%), Space: 17.7 MB (55.33%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -10,9 +10,11 @@ class Solution:
         def postorder(node):
             if not node:
                 return []
+            
             postorder(node.left)
             postorder(node.right)
             res.append(node.val)
         postorder(root)
         return res
+            
         
