b7a8f4b61d46f6379c27a88d3d719a08f81f7fa0
Akhil <akhil.kuchibhotla2006@gmail.com>
2025-09-02 23:56:36 +0530

    Time: 4 ms (68.41%), Space: 18.4 MB (90.32%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,5 +1,7 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        if not n:
+            return[n]
         ans = [0]*(n + 1)
         for i in range(1 , n + 1):
             ans[i] = ans[i >> 1] +  (i & 1)
