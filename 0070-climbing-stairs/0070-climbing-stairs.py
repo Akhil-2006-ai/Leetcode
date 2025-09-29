76e6ffc3d3b267515aa24837995780cb62787f22
Akhil <akhil.kuchibhotla2006@gmail.com>
2025-09-29 13:46:25 +0530

    Time: 0 ms (100%), Space: 17.5 MB (95.67%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,8 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        if not n:
+            return None
         one , two = 1 , 1
         for i in range(n - 1):
             temp = one
-            one = one + two
-            two = temp
+            one = one + two 
+            two = temp 
         return one
+        
